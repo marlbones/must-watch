d34bd32f17976a06910c342af42f74c7d7938832
marlbones <mdimaanods@gmail.com>
2019-05-11 01:32:41 +1000

    Added Image

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 import Colors from '../constants/Colors';
+import LoadingImage from '../components/LoadingImage';
 
 const AboutScreen = () => (
   <View style={styles.container}>
+    <View style={styles.imageContainer}>
+      <LoadingImage source={require('../assets/images/mustwatchlogo.png')} style={styles.logo}/>
+    </View>
     <View style={styles.textView}>
       <Text style={styles.text}>
         Must-watch is made by marlbones. Search a movie, add it to your list. Now you have a visual movie list!
       </Text>
       <Text style={styles.text}>
-        This app is a work in progress. Video game and book lists to come.
+        This app is a work in progress. Video game, book lists and other amazing features to come.
       </Text>
     </View>
   </View>
@@ -28,8 +32,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: 16,
   },
-  textView: {
+  imageContainer: {
     paddingTop: 40,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  textView: {
+    paddingTop: 16,
   },
   text: {
     color: Colors.blackText,
