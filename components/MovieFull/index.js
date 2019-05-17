9abae974b5eae0d543fa96de40e6597860abd63f
marlbones <mdimaanods@gmail.com>
2019-05-18 00:03:54 +1000

    fixed tomato bug

--- components/MovieFull/index.js
+++ components/MovieFull/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Alert, Animated } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Alert, Animated, Image } from 'react-native';
 import { Button } from 'react-native-elements';
 import { Icon } from 'expo';
 
@@ -96,7 +96,7 @@ const MovieFull = ({
             )}
             {(movie.Ratings.length > 1 && movie.Ratings[1].Source === "Rotten Tomatoes") && (
               <View style={styles.iconWrapper}>
-              <LoadingImage source={require('../../assets/images/tomato.png')} style={styles.rottenTomatoesIcon}/>
+              <Image source={require('../../assets/images/tomato.png')} style={styles.rottenTomatoesIcon}/>
               <Text style={styles.ratingText}> {movie.Ratings[1].Value}</Text>
               </View>
             )}
