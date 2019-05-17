8583f1b6095435a2a09f5746c8de1423398a286f
marlbones <mdimaanods@gmail.com>
2019-05-18 00:05:22 +1000

    Text for Seen List

--- screens/SeenScreen.js
+++ screens/SeenScreen.js
@@ -16,7 +16,7 @@ const SeenScreen = ({ device }) => (
       renderItem={({ item }) => <SeenListItem movie={item} />}
       ListEmptyComponent={
         <EmptyListMessage 
-          text={"Currently no items in Seen List"} 
+          text={"Currently no have been watched"} 
           containerStyle={styles.emptyStyle} 
         />
       }
